aa319ce2b8bf78a1287b41adf48cc4cb54fd4a72
david fletcher <davidfletcheraaron@gmail.com>
2022-01-21 09:25:12 -0500

    resource assignments are seeded

---
+++ data/seeds/04-resource-assignment.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('project_resources').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('project_resources').insert([
+        { project_id: 1, resource_id: 1 },
+        { project_id: 2, resource_id: 3 },
+        { project_id: 2, resource_id: 2 },
+        { project_id: 3, resource_id: 3 },
+        { project_id: 3, resource_id: 2 },
+        { project_id: 1, resource_id: 1 },
+      ]);
+    });
+};
