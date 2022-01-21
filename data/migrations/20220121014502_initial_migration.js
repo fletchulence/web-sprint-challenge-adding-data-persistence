04d1d0b65737267b64c06fdd0d9c709533e862f0
david fletcher <davidfletcheraaron@gmail.com>
2022-01-20 23:40:39 -0500

    fixed package.json and migration file

--- data/migrations/20220121014502_initial_migration.js
+++ data/migrations/20220121014502_initial_migration.js
@@ -3,7 +3,43 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema
+   .createTable('projects', tbl =>{
+     tbl.increments('project_id')
+     tbl.text('project_name').notNullable()
+     tbl.text('project_description')
+     tbl.boolean('project_completed').defaultTo(false)
+  })
+  .createTable('resources', tbl =>{
+     tbl.increments('resource_id')
+     tbl.text('resource_name').unique().notNullable()
+     tbl.text('resource_description')
+  })
+  .createTable('tasks', tbl =>{
+     tbl.increments('task_id')
+     tbl.text('task_description').notNullable()
+     tbl.text('task_notes')
+     tbl.boolean('task_completed').defaultTo(false)
+     tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('project_id').inTable('projects')
+  })
+  .createTable('project_resources', tbl => {
+     tbl.increments('project_resources_id')
+     tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('project_id').inTable('projects')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+     tbl.integer('resource_id')
+      .unsigned()
+      .notNullable()
+      .references('resource_id').inTable('resources')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
 };
 
 /**
@@ -11,5 +47,9 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema
+   .dropTableIfExists('project_resources')
+   .dropTableIfExists('tasks')
+   .dropTableIfExists('resources')
+   .dropTableIfExists('projects')
 };
