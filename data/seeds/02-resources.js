c8c52eca228f66c7f509d0d4acda673262dc5759
david fletcher <davidfletcheraaron@gmail.com>
2022-01-20 23:53:10 -0500

    seeding resources

---
+++ data/seeds/02-resources.js
@@ -0,0 +1,16 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('resources').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('resources').insert([
+        { "resource_name": "github", "resource_description": null},
+        { "resource_name": "dictionary.com", "resource_description": 'websters rendition of the most popular words'},
+        { "resource_name": "wiki.gov", "resource_description": 'mostly bs from the government'},
+      ]);
+    });
+};
