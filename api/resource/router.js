8619bef24705cac4356a4463941a1040ba234f17
david fletcher <davidfletcheraaron@gmail.com>
2022-01-21 09:54:46 -0500

    routers are set up at base structure

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,15 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+
+const Resource = require('./model')
+
+router.get('/', (req, res, next)=>{
+   Resource.getAll()
+   res.json({ message: 'working'})
+})
+
+router.post('/', (req, res, next)=>{
+
+})
+
+module.exports = router
