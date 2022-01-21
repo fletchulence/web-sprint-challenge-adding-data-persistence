d0276724ac44c97b099c68a4751fc1eed91d9171
david fletcher <davidfletcheraaron@gmail.com>
2022-01-21 09:47:16 -0500

    all routers routed are within the server file

--- api/server.js
+++ api/server.js
@@ -4,6 +4,13 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
+const projectsRouter = require('./project/router')
+const resourcesRouter = require('./resource/router')
+const tasksRouter = require('./task/router')
+
+server.use('/api/projects', projectsRouter)
+server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', tasksRouter)
 
 server.use((err, req, res, next) =>{
    res.status(err.status || 500).json({
