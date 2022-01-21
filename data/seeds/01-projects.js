9f086584561d425ee122153b736461a5ec1fdd20
david fletcher <davidfletcheraaron@gmail.com>
2022-01-20 23:52:59 -0500

    seeding projects

---
+++ data/seeds/01-projects.js
@@ -0,0 +1,16 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('projects').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('projects').insert([
+        { "project_name":"foo", "project_description": null, "project_completed":false },
+        { "project_name":"bar", "project_description": 'Sounds like a Cliff Bar snack -- i love those!',"project_completed": true },
+        { "project_name":"baz", "project_description": 'no idea what baz is supposed to mean', "project_completed": false },
+      ]);
+    });
+};
